85d8e5c7ba2b319f8a3d80c794f453cc8ea3268f
saifDeveloper <saikhsaifali@gmail.com>
2017-08-23 15:29:11 +0530

    greeting program

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	hourOfDay := time.Now().Hour()
+	greeting := getGreeting(hourOfDay)
+	fmt.Println(greeting)
+}
+func getGreeting(hour int) string {
+	if hour < 12 {
+		return "Good Morning!"
+	} else if hour < 18 {
+		return "Good Afternoon!"
+	} else {
+		return "good Evening!"
+	}
+}
